86de0d43ebd746b0efe627b5d69c61b4a0a4f38c
swankham <s.wankham@gmail.com>
2015-05-06 12:28:40 +0700

    Add Mark up form

---
+++ Erp.Custom/Erp.Custom.UI.Common/Views/SaleQuote/MarkUpPrice.cs
@@ -0,0 +1,29 @@
+﻿using Erp.Custom.Session.Models;
+using System.Windows.Forms;
+
+namespace Erp.Custom.UI.Common.Views.SaleQuote
+{
+    public partial class MarkUpPrice : baseInitialize
+    {
+        public MarkUpPrice(CustomSession _session = null, string param = null)
+        {
+            InitializeComponent();
+        }
+
+        private void trvMenu_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+        }
+
+        private void SetBillToGrid()
+        {
+            dgvList.Rows.Clear();
+
+            dgvList.Rows.Add("1", "T00014", "Magnetic motor starter 22 Kw (Y-D)", "10", "Psc", "100.00", "120.00", true);
+        }
+
+        private void MarkUpPrice_Load(object sender, System.EventArgs e)
+        {
+            SetBillToGrid();
+        }
+    }
+}
